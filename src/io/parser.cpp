5699af484d36b3fb4b401d94452a91a4a5f33076
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2020-02-21 09:02:22 +0100

    removed lexer

--- src/io/parser.cpp
+++ src/io/parser.cpp
@@ -2,16 +2,15 @@
 // Created by pierreantoine on 21/02/2020.
 //
 
+#include <istream>
 #include "parser.h"
 
 
 problem_instance parser::parse(std::istream & istream)
 {
-    const auto book_count = lexer::read_word(istream).second;
-    istream >> std::ws;
-    const auto libraries = lexer::read_word(istream).second;
-    istream >> std::ws;
-    const auto duration = lexer::read_word(istream).second;
+    const auto book_count = istream.get();
+    const auto libraries = istream.get();
+    const auto duration = istream.get();
 
     return problem_instance(book_count, libraries, duration);
 }
--- src/io/parser.h
+++ src/io/parser.h
@@ -6,7 +6,6 @@
 #pragma once
 
 
-#include "lexer.h"
 #include "../problem/problem_instance.h"
 
 
